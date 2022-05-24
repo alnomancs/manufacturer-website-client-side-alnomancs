ba83d2a5c8e833da833db20c6897daef71ac1e49
alnomancs <alnoman.cs@gmail.com>
2022-05-24 02:55:51 +0100

    create a header simple navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import Home from "./Home";
+import Home from "./Pages/Home/Home";
+import Login from "./Pages/Login/Login";
+import Navbar from "./Pages/Shared/Navbar";
+import Signup from "./Pages/Signup/Signup";
 
 function App() {
   return (
     <div className="App">
-      <Home></Home>
+      <Navbar></Navbar>
+
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="/signup" element={<Signup></Signup>}></Route>
+      </Routes>
     </div>
   );
 }
